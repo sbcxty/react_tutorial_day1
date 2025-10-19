2fe92daaac26c6fc365ce0a11e1c4fdcd84ec261
sbcxty <bagaoshekainah@gmail.com>
2025-10-19 20:53:54 +0800

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 export default function App() {
 
   return (
-    <div>Hello World</div>
+    <div className="flex flex-col min-h-screen">
+      <header className="bg-pink-200 px-3 py-3 text-center">Header</header>
+      <main className="bg-purple-300 grow p-3 text-center content-center text-6xl font-bold">Main</main>
+      <footer className="bg-blue-400 p-3  text-center">Footer</footer>
+    </div>
   )
 }
